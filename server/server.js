8d9a49df41769e49617a3a5eaa984fffefafec6a
Abhinav-Sharma01 <qrcashwani@gmail.com>
2026-02-26 14:10:18 +0530

    seeting files for vercel deployment

--- server/server.js
+++ server/server.js
@@ -17,13 +17,21 @@ import adminRoutes from "./routes/admin.routes.js";
 import notificationRoutes from "./routes/notification.routes.js";
 
 const app = express();
-const allowedOrigin =
-  process.env.FRONTEND_URL ||
-  "http://localhost:5173" ||
-  "https://tutoring-website-one-omega.vercel.app/";
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+  "https://tutoring-website-one-omega.vercel.app/"
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: allowedOrigin,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
@@ -52,6 +60,11 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+}
+
+export default app;
